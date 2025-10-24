66ed690376d7c470318d2a8e01d709620adf39c5
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-10-24 14:23:38 -0300

    Update

--- Java_Basic/Calculator.java
+++ Java_Basic/Calculator.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
-        int op = 0, n1, n2, res;
+        int op = 0;
+        int[] array= new int[3];
 
         Scanner sc = new Scanner(System.in);
 
         while (op != 5) {
-            clear();
+
             System.out.println("-----------------Calculator-----------------");
             System.out.println("1) Add\n2) Sub\n3) Mult\n4) Div\n5) Exit");
             System.out.println("--------------------------------------------");
@@ -24,47 +25,47 @@ public class Calculator {
                 case 1:
                     clear();
                     System.out.print("Enter First Number: ");
-                    n1 = sc.nextInt();
+                    array[0] = sc.nextInt();
                     System.out.print("Enter Second Number: ");
-                    n2 = sc.nextInt();
-                    res = n1 + n2;
-                    System.out.println("Result: " + res);
+                    array[1] = sc.nextInt();
+                    array[2] = array[0] + array[1];
+                    System.out.println("Result: " + array[2]);
                     break;
 
                 case 2:
                     clear();
 
                     System.out.print("Enter First Number: ");
-                    n1 = sc.nextInt();
+                    array[0] = sc.nextInt();
                     System.out.print("Enter Second Number: ");
-                    n2 = sc.nextInt();
-                    res = n1 - n2;
-                    System.out.println("Result: " + res);
+                    array[1] = sc.nextInt();
+                    array[2] = array[0] - array[1];
+                    System.out.println("Result: " + array[2]);
                     break;
 
                 case 3:
                     clear();
 
                     System.out.print("Enter First Number: ");
-                    n1 = sc.nextInt();
+                    array[0] = sc.nextInt();
                     System.out.print("Enter Second Number: ");
-                    n2 = sc.nextInt();
-                    res = n1 * n2;
-                    System.out.println("Result: " + res);
+                    array[1] = sc.nextInt();
+                    array[2] = array[0] * array[1];
+                    System.out.println("Result: " + array[2]);
                     break;
 
                 case 4:
                     clear();
 
                     System.out.print("Enter First Number: ");
-                    n1 = sc.nextInt();
+                    array[0]= sc.nextInt();
                     System.out.print("Enter Second Number: ");
-                    n2 = sc.nextInt();
-                    if (n2 == 0) {
+                    array[1] = sc.nextInt();
+                    if (array[1] == 0) {
                         System.out.println("Division By Zero");
                     } else {
-                        res = n1 / n2;
-                        System.out.println("Result: " + res);
+                        array[2] = array[0] / array[1];
+                        System.out.println("Result: " + array[2]);
                     }
                     break;
 
@@ -75,8 +76,7 @@ public class Calculator {
         }
     }
     public static void clear() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        for (int i = 0; i < 50; i++) System.out.println();
     }
 }
 
